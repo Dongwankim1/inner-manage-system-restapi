c81595130612b01adb94520839067f3d3b575e11
dongwan <zkdlwu94@naver.com>
2022-01-27 18:15:52 +0900

    a

--- src/main/java/com/ims/InnerManageSystemRestApplication.java
+++ src/main/java/com/ims/InnerManageSystemRestApplication.java
@@ -1,22 +1,15 @@
 package com.ims;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.ims.com.security.SpringSecurityConfig;
 
 @SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 public class InnerManageSystemRestApplication extends SpringBootServletInitializer{
 	
 	@Bean
@@ -29,12 +22,5 @@ public class InnerManageSystemRestApplication extends SpringBootServletInitializ
 	public static void main(String[] args) {
 		SpringApplication.run(InnerManageSystemRestApplication.class, args);
 	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return builder.sources(InnerManageSystemRestApplication.class);
-	}
-	
-	private static Class<InnerManageSystemRestApplication> applicationClass = InnerManageSystemRestApplication.class;
+
 }
