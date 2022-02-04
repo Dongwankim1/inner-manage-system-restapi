724a1bd1e245cc6e0c8e36b0614d254381d3fa80
dongwan <zkdlwu94@naver.com>
2022-02-04 11:47:08 +0900

    encrypt mysql id,password in application.xml

--- src/test/java/com/ims/app/InnerManageSystemRestApplicationTests.java
+++ src/test/java/com/ims/app/InnerManageSystemRestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.ims.app;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InnerManageSystemRestApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
-	}
+		
+		String url = "localhost:3306/ims";
+        String username = "ims";
+        String password = "ims12!@";
 
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+	}
+	
+	
+	public String jasyptEncoding(String value) {
+
+        String key = "my_name_dizzy";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
 }
